84835eb3317a7113ce5c1677180a510ed1cf0fa1
anukulKun <anukulpandit30s@gmail.com>
2024-12-30 11:22:21 +0530

    Link added

--- components/BlueBox.tsx
+++ components/BlueBox.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function BlueBox() {
   return (
-    <div className="w-[275.14px] absolute aspect-square right-[170px] bottom-[90px] scale-50 md:scale-75 lg:scale-100 flex justify-evenly items-center ">
-      <Image
-        src="/bluee.svg"
-        className="w-full h-full"
-        width={135.91}
-        height={135.91}
-        alt="disk-inner"
-      />
+    <div className="w-[275.14px] absolute aspect-square right-[170px] bottom-[90px] scale-50 md:scale-75 lg:scale-100 flex justify-evenly items-center">
+      <Link href="https://magiceden.io/launchpad/squad_game">
+        
+          <Image
+            src="/bluee.svg"
+            className="w-full h-full"
+            width={135.91}
+            height={135.91}
+            alt="disk-inner"
+          />
+      
+      </Link>
     </div>
   );
 }
