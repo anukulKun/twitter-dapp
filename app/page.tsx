37d43e00ebadd8e3b156f46b49142c7efa6f4e66
anukulKun <anukulpandit30s@gmail.com>
2024-12-30 08:38:38 +0530

    pushing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+""
 import AnimatedLoader from "@/components/animated-loader";
 import BlueBox from "@/components/BlueBox";
 import CyberLoader from "@/components/cyber-loader";
@@ -7,6 +8,7 @@ import House from "@/components/house";
 import PoweredBy from "@/components/powered-by";
 import PrizeWrap from "@/components/prize-wrap";
 import RadarChart from "@/components/radar-chart";
+import Model from "@/components/Model";
 
 export default function Home() {
 	return (
@@ -19,6 +21,7 @@ export default function Home() {
 			<RadarChart />
 			<AnimatedLoader />
             <EstimatedTime />
+			<Model/>
 		</Frame>
 	);
 }
